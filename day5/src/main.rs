5f2effce6e7810e86dfb533536e2d115acd83d03
Christian Kramer <ck.daill@gmail.com>
2025-12-06 14:08:47 +0100

    transfer and day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,37 +1,91 @@
 use std::collections::HashSet;
-use std::io::{self};
+use std::io::{self, Cursor};
+use std::ops::RangeInclusive;
 use std::{fs::File, io::BufRead};
 
-fn read_from_file() -> HashSet<(isize, isize)> {
+fn read_from_file() -> (Vec<(u64, u64)>, Vec<u64>) {
     let file = File::open("inputs");
-    let result: HashSet<(isize, isize)> = match file {
+    let result: (Vec<(u64, u64)>, Vec<u64>) = match file {
         Ok(file) => {
             let lines = io::BufReader::new(file).lines();
-            let mut result: HashSet<(isize, isize)> = HashSet::new();
+            let mut ranges: Vec<(u64, u64)> = Vec::new();
+            let mut ing: Vec<(u64)> = Vec::new();
             for (id, line) in lines.enumerate() {
                 if let Ok(line) = line {
-                    line.chars().enumerate().for_each(|(i, x)| {
-                        if x == '@' {
-                            let c = ((id as isize), (i as isize));
-                            result.insert(c.clone());
+                    if line.len() != 0 {
+                        if line.contains('-') {
+                            let mut parts = line.split('-');
+                            let d = parts.next().unwrap();
+                            let u = parts.next().unwrap();
+                            ranges.push((d.parse().unwrap(), u.parse().unwrap()));
+                        } else {
+                            ing.push(line.parse().unwrap());
                         }
-                    });
+                    }
                 }
             }
-            result
+            (ranges, ing)
         }
         Err(e) => panic!("Cannot process file: {}", e),
     };
     result
 }
 
-fn do_task_one(inputs: &HashSet<(isize, isize)>) {
-    let mut result = process(&inputs);
-    println!("{:?}", result.len());
+fn do_task_two(inputs: &mut (Vec<(u64, u64)>, Vec<u64>)) {
+    let mut ranges = inputs.0.clone();
+    ranges.sort_by_key(|rang| (rang.0, rang.1));
+    let mut last_range = ranges.first().unwrap();
+    let mut res = last_range.1 - last_range.0;
+    let mut las_top = last_range.1;
+    for i in 1..ranges.len() {
+        let cur_range = ranges.get(i).unwrap();
+        if cur_range.1 > las_top && cur_range.0 < las_top {
+            res += cur_range.1 - las_top;
+        } else {
+            if cur_range.0 >= last_range.0
+                && cur_range.0 <= last_range.1
+                && cur_range.1 > last_range.1
+            {
+                res += cur_range.1 - last_range.1;
+            } else if cur_range.0 >= last_range.1 {
+                if cur_range.0 == last_range.1 {
+                    res += cur_range.1 - cur_range.0;
+                } else {
+                    res += cur_range.1 - cur_range.0 + 1;
+                }
+            }
+        }
+        println!("{:?} {:?} {:?}", last_range, cur_range, res);
+        if cur_range.1 > las_top {
+            las_top = cur_range.1;
+        }
+        last_range = cur_range;
+    }
+    println!("{:?}", ranges);
+    println!("{:?}", res);
+}
+
+fn do_task_one(inputs: &mut (Vec<(u64, u64)>, Vec<u64>)) {
+    let mut res = 0;
+    let mut ings = inputs.1.to_owned();
+    for range in &inputs.0 {
+        let r = RangeInclusive::new(range.0, range.1);
+        ings.retain(|ing| {
+            println!("proc {:?}", ing);
+            if r.contains(&ing) {
+                println!("fresh {:?}", ing);
+                res += 1;
+                false
+            } else {
+                true
+            }
+        });
+    }
+    println!("{:?}", res);
 }
 
 fn main() {
-    let inputs = read_from_file();
-    do_task_two(&inputs);
+    let mut inputs = read_from_file();
+    do_task_two(&mut inputs);
     // println!("{:?}", &inputs);
 }
---
+++ day6/src/main.rs
@@ -0,0 +1,121 @@
+use std::collections::HashSet;
+use std::io::{self, Cursor};
+use std::iter::Enumerate;
+use std::num;
+use std::ops::RangeInclusive;
+use std::{fs::File, io::BufRead};
+
+fn read_from_file() -> (Vec<Vec<String>>, Vec<String>, Vec<usize>) {
+    let file = File::open("test");
+    let result: (Vec<Vec<String>>, Vec<String>, Vec<usize>) = match file {
+        Ok(file) => {
+            let mut lines: Vec<String> = Vec::new();
+            io::BufReader::new(file)
+                .lines()
+                .for_each(|f| lines.push(f.unwrap()));
+            let mut ops: Vec<String> = Vec::new();
+            let mut col_sizes: Vec<usize> = Vec::new();
+            let mut numbers: Vec<Vec<String>> = Vec::new();
+            let mut col_index = 0;
+
+            for (id, line) in lines.iter().enumerate().rev() {
+                if line.contains("*") || line.contains("+") {
+                    let mut count = 0;
+                    line.chars().enumerate().for_each(|(i, f)| {
+                        if f.is_whitespace() {
+                            count += 1;
+                        } else {
+                            ops.push(f.to_string());
+                            if i != 0 {
+                                col_sizes.push(count);
+                                count = 0;
+                            }
+                        }
+                    });
+                    col_sizes.push(count + 1);
+                } else {
+                    let mut nums: Vec<String> = Vec::new();
+
+                    let mut chars: Vec<char> = line.chars().collect();
+                    for i in 0..col_sizes.len() {
+                        let mut size = col_sizes[i];
+                        let (num, rest) = chars.split_at_mut(size);
+                        println!("{:?} {:?}", num, rest);
+                        nums.push(num.iter().collect::<String>());
+                        chars = Vec::from(rest);
+                        if chars.len() > 0 {
+                            chars.remove(0);
+                        }
+                    }
+                    numbers.push(nums);
+                }
+            }
+            (numbers, ops, col_sizes)
+        }
+        Err(e) => panic!("Cannot process file: {}", e),
+    };
+    result
+}
+
+fn do_task_two(inputs: (Vec<Vec<String>>, Vec<String>, Vec<usize>)) {
+    println!("{:?} {:?} {:?}", inputs.0, inputs.1, inputs.2);
+    let mut lines = inputs.0.clone();
+    let mut colums = vec![];
+    lines.reverse();
+    for i in 0..inputs.2.len() {
+        let mut colum = vec![];
+        for line in &lines {
+            colum.push(line[i].clone());
+        }
+        colums.push(colum);
+    }
+
+    println!("{:?}", colums);
+
+    let mut new_nums: Vec<Vec<char>> = Vec::new();
+
+    for col in colums {
+        println!("{:?}", col);
+        let l = col.first().unwrap().len();
+
+        for i in 0..l {
+            for num in &col {
+                if i == 0 {
+                    new_nums.push(Vec::new());
+                }
+                new_nums[i].push(num.chars().nth(i).unwrap());
+                println!("{:?}", new_nums[i]);
+            }
+        }
+        println!("{:?}", new_nums);
+    }
+}
+
+fn do_task_one(inputs: (Vec<Vec<String>>, Vec<String>, Vec<usize>)) {
+    let cols = inputs.2.len();
+    let mut res: Vec<u64> = vec![0; cols];
+    let ops = inputs.1;
+    for (i, n) in inputs.0.iter().enumerate() {
+        for j in 0..cols {
+            let num = n[j].parse().unwrap();
+            println!("{:?} {:?} {:?} {:?}", num, res[j], i, j);
+            if ops[j] == "+" {
+                res[j] += num;
+            } else if ops[j] == "*" {
+                if res[j] != 0 {
+                    res[j] *= num;
+                } else {
+                    res[j] = num;
+                }
+            }
+        }
+    }
+    let sum: u64 = res.iter().sum();
+    println!("{:?}", sum);
+}
+
+fn main() {
+    let mut inputs = read_from_file();
+    do_task_two(inputs);
+    // println!("{:?}", &inputs);
+}
